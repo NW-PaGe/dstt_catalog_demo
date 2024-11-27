c81ed52ada6cd10b7dfb4db8fa8a5145a4c2c391
Philip Crain <philip.crain@doh.wa.gov>
2024-11-27 14:13:38 -0800

    chore: remove test metadata and use prod version
    closes #45

--- src/dag.R
+++ src/dag.R
@@ -3,86 +3,8 @@ library(DiagrammeR)
 library(dplyr)
 library(tidyr)
 
-# Sample data frame with node and edge information
-metadata <- structure(
-  list(
-    Product_ID = c(4047648790, 5754199350, 9000442265, 
-                   9325481063, 6072493491, 5645041168, 3344276255, 2307679692, 1512337206, 
-                   2824302477, 3748996607, 2008766938, 7300648833, 3855770051, 3092745195, 
-                   4520627002, 3932969825, 6873647526, 8109343197, 9323872357, 7334703566, 
-                   8858668888, 7829333381, 9936639753, 2463472188, 8971627055, 7253259313, 
-                   9715465027, 8085881829, 7394149033, 3590275606), 
-    Product_Name = c("wa_genbank", 
-                     "wa_genbank", "wa_genbank", "wa_genbank", "lineages", "lineages", 
-                     "lineages", "lineages", "lineages", "lineages", "lineages", "lineages", 
-                     "lineages", "lineages", "lineages", "lineages", "lineages", "lineages", 
-                     "lineages", "lineages", "lineages", "lineages", "seq_results", 
-                     "seq_results", "seq_results", "seq_results", "seq_report", "genbank", 
-                     "genbank", "genbank", "genbank"), 
-    Location = c("network_drive", 
-                 "network_drive", "network_drive", "network_drive", "network_drive", 
-                 "network_drive", "network_drive", "network_drive", "network_drive", 
-                 "network_drive", "network_drive", "network_drive", "network_drive", 
-                 "network_drive", "network_drive", "network_drive", "network_drive", 
-                 "network_drive", "network_drive", "network_drive", "network_drive", 
-                 "network_drive", "local", "local", "local", "local", "network_drive", 
-                 "network_drive", "network_drive", "network_drive", "network_drive"), 
-    Steward = c("DIQA", "DIQA", "DIQA", "DIQA", "DIQA", "DIQA", 
-                "DIQA", "DIQA", "DIQA", "DIQA", "DIQA", "DIQA", "DIQA", "DIQA", 
-                "DIQA", "DIQA", "DIQA", "DIQA", "DIQA", "DIQA", "DIQA", "DIQA", 
-                "PHL", "PHL", "PHL", "PHL", "MEP", "DIQA", "DIQA", "DIQA", "DIQA"), 
-    Source = c("genbank", "genbank", "genbank", "genbank", "CDC", 
-               "CDC", "CDC", "CDC", "CDC", "CDC", "CDC", "CDC", "CDC", "CDC", 
-               "CDC", "CDC", "CDC", "CDC", "CDC", "CDC", "CDC", "CDC", "BINFX", 
-               "BINFX", "BINFX", "BINFX", "", "NCBI", "NCBI", "NCBI", "NCBI"), 
-    Users = c("DIQA; MEP", "DIQA; MEP", "DIQA; MEP", "DIQA; MEP", 
-              "MEP", "MEP", "MEP", "MEP", "MEP", "MEP", "MEP", "MEP", "MEP", 
-              "MEP", "MEP", "MEP", "MEP", "MEP", "MEP", "MEP", "MEP", "MEP", 
-              "PHL", "PHL", "PHL", "PHL", "Public", "DIQA", "DIQA", "DIQA", 
-              "DIQA"), 
-    Keywords = c("ncbi;results;accessions;virus;refseq;upload", 
-                 "ncbi;results;accessions;virus;refseq;upload", "ncbi;results;accessions;virus;refseq;upload", 
-                 "ncbi;results;accessions;virus;refseq;upload", "variant", "variant", 
-                 "variant", "variant", "variant", "variant", "variant", "variant", 
-                 "variant", "variant", "variant", "variant", "variant", "variant", 
-                 "variant", "variant", "variant", "variant", "sequencing;results;accessions;variants;lineages", 
-                 "sequencing;results;accessions;variants;lineages", "sequencing;results;accessions;variants;lineages", 
-                 "sequencing;results;accessions;variants;lineages", "report:variants;lineages", 
-                 "ncbi;genbank;lineages;repository;refseq", "ncbi;genbank;lineages;repository;refseq", 
-                 "ncbi;genbank;lineages;repository;refseq", "ncbi;genbank;lineages;repository;refseq"), 
-    PII = c("No", "No", "No", "No", "No", "No", "No", "No", "No", 
-            "No", "No", "No", "No", "No", "No", "No", "No", "No", "No", "No", 
-            "No", "No", "No", "No", "No", "No", "No", "No", "No", "No", "Yes"), 
-    Connection = c("data/wa_genbank/wa_genbank_2024-10-21.csv", 
-                   "data/wa_genbank/wa_genbank_2024-10-14.csv", "data/wa_genbank/wa_genbank_2024-10-07.csv", 
-                   "data/wa_genbank/wa_genbank_2024-09-30.csv", "data/lineages/Lineages_2024-06-03.csv", 
-                   "data/lineages/Lineages_2024-06-04.csv", "data/lineages/Lineages_2024-06-05.csv", 
-                   "data/lineages/Lineages_2024-06-06.csv", "data/lineages/Lineages_2024-06-07.csv", 
-                   "data/lineages/Lineages_2024-06-10.csv", "data/lineages/Lineages_2024-06-11.csv", 
-                   "data/lineages/Lineages_2024-06-12.csv", "data/lineages/Lineages_2024-06-13.csv", 
-                   "data/lineages/Lineages_2024-06-14.csv", "data/lineages/Lineages_2024-06-17.csv", 
-                   "data/lineages/Lineages_2024-06-18.csv", "data/lineages/Lineages_2024-06-19.csv", 
-                   "data/lineages/Lineages_2024-06-20.csv", "data/lineages/Lineages_2024-06-21.csv", 
-                   "data/lineages/Lineages_2024-06-24.csv", "data/lineages/Lineages_2024-06-25.csv", 
-                   "data/lineages/Lineages_2024-06-26.csv", "data/seq_results/results_with_ncbi_run163.csv", 
-                   "data/seq_results/results_with_ncbi_run170.csv", "data/seq_results/results_with_ncbi_run172.csv", 
-                   "data/seq_results/results_with_ncbi_run172b.csv", "data/seq_report/SequencingReport_2024-01-30.docx", 
-                   "data/genbank/raw_genbank_2024-09-30.csv", "data/genbank/raw_genbank_2024-10-07.csv", 
-                   "data/genbank/raw_genbank_2024-10-14.csv", "data/genbank/raw_genbank_2024-10-21.csv"), 
-    parent = c("genbank", "genbank", "genbank", "genbank", NA, 
-               NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, 
-               NA, NA, NA, NA, NA, "seq_results;lineages;wa_genbank", NA, NA, 
-               NA, NA), 
-    child = c("seq_report", "seq_report", "seq_report", 
-              "seq_report", "seq_report", "seq_report", "seq_report", "seq_report", 
-              "seq_report", "seq_report", "seq_report", "seq_report", "seq_report", 
-              "seq_report", "seq_report", "seq_report", "seq_report", "seq_report", 
-              "seq_report", "seq_report", "seq_report", "seq_report", "seq_report", 
-              "seq_report", "seq_report", "seq_report", NA, "wa_genbank", "wa_genbank", 
-              "wa_genbank", "wa_genbank")
-  ), 
-  class = "data.frame", row.names = c(NA, -31L)
-)
+# Read the CSV file when the script starts
+metadata <- read.csv(url("https://raw.githubusercontent.com/NW-PaGe/dstt_catalog_demo/refs/heads/main/metadata.csv"))
 
 # Define UI for the app
 ui <- fluidPage(
@@ -113,43 +35,43 @@ server <- function(input, output) {
       mutate(Product_Name_Copy = Product_Name) %>% 
       distinct()
     
-    ### Create edges based on `parent` and `child` columns ----------------
+    ### Create edges based on `Parent` and `Child` columns ----------------
     
     # unnest any nodes with 2+ parents or children
     unnested_data <- metadata %>% 
-      select(Product_Name, parent, child) %>% 
+      select(Product_Name, Parent, Child) %>% 
       mutate(Product_Name_Copy = Product_Name) %>% 
-      tidyr::separate_rows(parent, sep = ";") %>% 
-      tidyr::separate_rows(child, sep = ";") %>% 
+      tidyr::separate_rows(Parent, sep = ";") %>% 
+      tidyr::separate_rows(Child, sep = ";") %>% 
       distinct()
     
     # identify all possible parents
-    parent <- unnested_data %>% 
-      filter(!is.na(parent)) %>% 
-      mutate(child = Product_Name)
+    parent_df <- unnested_data %>% 
+      filter(!is.na(Parent)) %>% 
+      mutate(Child = Product_Name)
 
     # identify all possible children
-    child <- unnested_data %>% 
-      filter(!is.na(child)) %>% 
-      mutate(parent = Product_Name)
+    child_df <- unnested_data %>% 
+      filter(!is.na(Child)) %>% 
+      mutate(Parent = Product_Name)
     
     # join parents and children to get all possible edges
-    edges <- rbind(parent, child) %>% 
-      select(parent, child) %>% 
+    edges <- rbind(parent_df, child_df) %>% 
+      select(Parent, Child) %>% 
       distinct()
     
     # get direct edges (edges where no product has been deselected)
     direct_edges <- edges %>% 
-      filter(parent %in% input$selected_nodes & child %in% input$selected_nodes) %>% 
+      filter(Parent %in% input$selected_nodes & Child %in% input$selected_nodes) %>% 
       mutate(style = 'solid') # set edge attribute so line will be solid
     
     # get indirect edges (edges where child or parent has been deselected)
     indirect_parent <- edges %>% 
-      filter(!parent %in% input$selected_nodes) %>% 
-      rename(removed=parent)
+      filter(!Parent %in% input$selected_nodes) %>% 
+      rename(removed=Parent)
     indirect_child <- edges %>% 
-      filter(!child %in% input$selected_nodes) %>% 
-      rename(removed=child)
+      filter(!Child %in% input$selected_nodes) %>% 
+      rename(removed=Child)
     indirect_edges <- indirect_parent %>% 
       left_join(indirect_child, by='removed') %>% 
       select(-removed) %>% 
@@ -163,8 +85,8 @@ server <- function(input, output) {
       add_nodes_from_table(table = nodes_final,
                            label_col = "Product_Name") %>%
       add_edges_from_table(table = edges_final,
-                           from_col = "parent",
-                           to_col = "child",
+                           from_col = "Parent",
+                           to_col = "Child",
                            from_to_map = "Product_Name_Copy")
 
     return(dag)
